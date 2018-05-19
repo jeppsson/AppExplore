43805f2a7774459a1e7307866bfb3580dd7acb50
Mathias Jeppsson <mathias.jeppsson@gmail.com>
2018-05-20 00:45:13 +0900

    Check package updates periodically on O and above

---
+++ app/src/main/java/com/jeppsson/appexplore/PeriodicJobService.java
@@ -0,0 +1,41 @@
+package com.jeppsson.appexplore;
+
+import android.app.job.JobInfo;
+import android.app.job.JobParameters;
+import android.app.job.JobScheduler;
+import android.app.job.JobService;
+import android.content.ComponentName;
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+public class PeriodicJobService extends JobService {
+
+    private static final long PERIOD = 2 * 24 * 60 * 60 * 1000;
+
+    static void enqueueWork(Context context) {
+        JobInfo.Builder builder = new JobInfo.Builder(1000,
+                new ComponentName(context, PeriodicJobService.class));
+        builder.setRequiresCharging(true)
+                .setPersisted(true)
+                .setPeriodic(PERIOD);
+
+        JobScheduler jobScheduler =
+                (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        if (jobScheduler != null) {
+            jobScheduler.schedule(builder.build());
+        }
+    }
+
+    @Override
+    public boolean onStartJob(JobParameters params) {
+        PackageScannerService.enqueueWork(this);
+        return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        return false;
+    }
+}

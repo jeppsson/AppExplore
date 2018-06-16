5b7627d2d70b0d6d6a79028e4de053aee7ef1b25
Mathias Jeppsson <mathias.jeppsson@gmail.com>
2018-06-16 23:15:03 +0900

    Move PackageListFragment creation to xml

--- app/src/main/java/com/jeppsson/appexplore/MainActivity.java
+++ app/src/main/java/com/jeppsson/appexplore/MainActivity.java
@@ -8,17 +8,10 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG_FRAGMENT = "PACKAGE_LIST_FRAGMENT";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fragment_container, new PackageListFragment(), TAG_FRAGMENT).commit();
-        }
 
         PackageScannerService.enqueueWork(this);
 

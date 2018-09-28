2558a82e019e0a1f73e317e820b623c835d62c65
Mathias Jeppsson <mathias.jeppsson@gmail.com>
2018-09-29 07:22:12 +0900

    Colorize application compontent properties

--- app/src/main/java/com/jeppsson/appexplore/utils/AppComponentUtils.kt
+++ app/src/main/java/com/jeppsson/appexplore/utils/AppComponentUtils.kt
@@ -1,6 +1,11 @@
 package com.jeppsson.appexplore.utils
 
 import android.content.pm.PackageInfo
+import android.graphics.Color
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import java.util.regex.Pattern
 
 internal object AppComponentUtils {
 
@@ -9,77 +14,83 @@ internal object AppComponentUtils {
     private const val LAUNCH_SINGLE_TASK = 2
     private const val LAUNCH_SINGLE_INSTANCE = 3
 
-    fun getActivities(packageInfo: PackageInfo): String {
+    fun getActivities(packageInfo: PackageInfo): Spannable {
         val sb = StringBuilder()
 
         if (packageInfo.activities != null) {
             for (activityInfo in packageInfo.activities) {
                 sb.append(activityInfo.name).append('\n')
+                val info = StringBuilder()
                 if (activityInfo.exported) {
-                    sb.append("Exported: true").append('\n')
+                    info.append("(exported)").append(' ')
                 }
                 if (activityInfo.launchMode != LAUNCH_MULTIPLE) {
-                    sb.append("LaunchMode: ")
+                    info.append('(')
                             .append(launchMode(activityInfo.launchMode))
-                            .append('\n')
+                            .append(')')
+                            .append(' ')
                 }
+                if (info.isNotEmpty()) {
+                    info.append('\n')
+                }
+                sb.append(info)
             }
         } else {
             sb.append("No activities")
         }
 
-        return sb.toString().trim()
+        return colorize(sb.toString().trim())
     }
 
-    fun getServices(packageInfo: PackageInfo): String {
+    fun getServices(packageInfo: PackageInfo): Spannable {
         val sb = StringBuilder()
 
         if (packageInfo.services != null) {
             for (serviceInfo in packageInfo.services) {
                 sb.append(serviceInfo.name).append('\n')
                 if (serviceInfo.exported) {
-                    sb.append("Exported: true").append('\n')
+                    sb.append("(exported)").append('\n')
                 }
             }
         } else {
             sb.append("No services")
         }
 
-        return sb.toString().trim()
+        return colorize(sb.toString().trim())
     }
 
-    fun getContentProviders(packageInfo: PackageInfo): String {
+    fun getContentProviders(packageInfo: PackageInfo): Spannable {
         val sb = StringBuilder()
 
         if (packageInfo.providers != null) {
             for (providerInfo in packageInfo.providers) {
                 sb.append(providerInfo.name).append('\n')
                 if (providerInfo.exported) {
-                    sb.append("Exported: true").append('\n')
+                    sb.append("(exported)").append('\n')
                 }
             }
         } else {
             sb.append("No content providers")
         }
 
-        return sb.toString().trim()
+        return colorize(sb.toString().trim())
     }
 
-    fun getReceivers(packageInfo: PackageInfo): String {
+    fun getReceivers(packageInfo: PackageInfo): Spannable {
         val sb = StringBuilder()
 
         if (packageInfo.receivers != null) {
             for (activityInfo in packageInfo.receivers) {
                 sb.append(activityInfo.name).append('\n')
                 if (activityInfo.exported) {
-                    sb.append("Exported: ").append('\n')
+                    sb.append("(exported)").append('\n')
                 }
             }
         } else {
             sb.append("No receivers")
         }
 
-        return sb.toString().trim()
+        return colorize(sb.toString().trim())
     }
 
 
@@ -91,5 +102,17 @@ internal object AppComponentUtils {
             LAUNCH_SINGLE_INSTANCE -> "singleInstance"
             else -> "unknown"
         }
+    }
+
+    private fun colorize(text: String): SpannableString {
+        val content = SpannableString(text)
+
+        val m = Pattern.compile("\\b(?:exported)\\b").matcher(text)
+        while (m.find()) {
+            content.setSpan(ForegroundColorSpan(Color.YELLOW),
+                    m.start(), m.end(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        }
+
+        return content
     }
 }

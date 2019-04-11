2516068d077b7203aaa8d31befb568203baadfd4
Mathias Jeppsson <mathias.jeppsson@gmail.com>
2019-04-11 09:38:03 +0900

    Fix StrictMode violation when listing native libs

--- app/src/main/java/com/jeppsson/appexplore/databinding/LibraryBindingAdapter.kt
+++ app/src/main/java/com/jeppsson/appexplore/databinding/LibraryBindingAdapter.kt
@@ -1,9 +1,11 @@
 package com.jeppsson.appexplore.databinding
 
 import android.content.pm.ApplicationInfo
+import android.os.AsyncTask
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import java.io.File
+import java.lang.ref.WeakReference
 
 @BindingAdapter("shared_libraries")
 fun sharedLibraries(view: TextView, applicationInfo: ApplicationInfo) {
@@ -15,12 +17,12 @@ fun sharedLibraries(view: TextView, applicationInfo: ApplicationInfo) {
 @BindingAdapter("native_libraries")
 fun nativeLibraries(view: TextView, applicationInfo: ApplicationInfo) {
     if (applicationInfo.nativeLibraryDir != null) {
-        view.text = arrayToLines(File(applicationInfo.nativeLibraryDir).list(),
-                applicationInfo.nativeLibraryDir)
+        NativeLibraryDir(WeakReference(view), applicationInfo.nativeLibraryDir)
+            .executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR)
     }
 }
 
-fun arrayToLines(strings: Array<String>?, prefix: String?): String {
+private fun arrayToLines(strings: Array<String>?, prefix: String?): String {
     val sb = StringBuilder()
 
     if (strings != null) {
@@ -30,4 +32,18 @@ fun arrayToLines(strings: Array<String>?, prefix: String?): String {
     }
 
     return sb.toString().trim()
+}
+
+private class NativeLibraryDir(
+    private val view: WeakReference<TextView>,
+    private val nativeLibraryDir: String
+) : AsyncTask<Void, Void, Array<String>?>() {
+
+    override fun doInBackground(vararg params: Void?): Array<String>? {
+        return File(nativeLibraryDir).list()
+    }
+
+    override fun onPostExecute(result: Array<String>?) {
+        view.get()?.text = arrayToLines(result, nativeLibraryDir)
+    }
 }
